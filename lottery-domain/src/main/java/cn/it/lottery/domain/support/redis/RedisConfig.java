4ce36d2fa6e243d8167a8d58251c2b208f08e224
Youwei <liyouwei654321@gmail.com>
2025-04-03 19:15:10 -0700

    comment change

--- lottery-domain/src/main/java/cn/it/lottery/domain/support/redis/RedisConfig.java
+++ lottery-domain/src/main/java/cn/it/lottery/domain/support/redis/RedisConfig.java
@@ -14,10 +14,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @description: Redis 配置类
- * @author: 小傅哥，微信：fustack
- * @date: 2021/11/20
- * @github: https://github.com/fuzhengwei
- * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  */
 @Configuration
 @EnableCaching
